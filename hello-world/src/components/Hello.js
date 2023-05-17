3db67e2ab9229418ffb65b62be992c70831648f9
Bilal9744 <126229390+Bilal9744@users.noreply.github.com>
2023-05-17 09:33:23 +0530

    Initial commit

---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    // return(
+    //     <div>
+    //         <h1>Hello vishwas</h1>
+    //     </div>
+    // )
+    return React.createElement('div',null,'<h1>Hello vishwas</h1>')
+}
+export default Hello
